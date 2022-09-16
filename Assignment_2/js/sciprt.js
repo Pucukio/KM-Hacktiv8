1898e232bfbc572b8674cc9d9ddaba2437132de2
pucuk1337 <pucuk4us@gmail.com>
2022-09-16 10:05:45 +0700

    Assignment 2 - Manipulate with JS

---
+++ Assignment_2/js/sciprt.js
@@ -0,0 +1,14 @@
+function change_name() {
+  const nama = document.getElementById("nama").value;
+}
+
+function change_profile() {
+  // const role = document.getElementById("role").value;
+  // const availability = document.getElementById("availability").value;
+  // const usia = document.getElementById("usia").value;
+  // const lokasi = document.getElementById("lokasi").value;
+  // const year_experience = document.getElementById("year_experience").value;
+  // const email = document.getElementById("email").value;
+
+  document.getElementById("namaLengkap").innerHTML = change_name();
+}
