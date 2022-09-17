6e38e4e82602009a4db8c6f6f1a6297f4b3cb72d
pucuk1337 <pucuk4us@gmail.com>
2022-09-18 05:41:37 +0700

    Assignment_2 - Fix Name

--- Assignment_2/js/script.js
+++ Assignment_2/js/script.js
@@ -1,18 +1,3 @@
-// function change_name() {
-//   const nama = document.getElementById("nama").value;
-// }
-
-// function change_profile() {
-//   // const role = document.getElementById("role").value;
-//   // const availability = document.getElementById("availability").value;
-//   // const usia = document.getElementById("usia").value;
-//   // const lokasi = document.getElementById("lokasi").value;
-//   // const year_experience = document.getElementById("year_experience").value;
-//   // const email = document.getElementById("email").value;
-
-//   document.getElementById("namaLengkap").innerHTML = change_name();
-// }
-
 const buttonSubmit = document.querySelector("#submit");
 const nama = document.querySelector("#nama");
 const namaInput = document.querySelector("#namaInput");
