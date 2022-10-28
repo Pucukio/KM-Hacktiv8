ecf2cca0f14b934d8d1e43fa5780e54071500c88
Rio Griya Putra <77945629+Pucuk1337@users.noreply.github.com>
2022-10-28 10:35:41 +0700

    Delete Sesi 10 - Review JavaScript directory

--- Sesi 10 - Review JavaScript/exercises3.js
+++
@@ -1,9 +0,0 @@
-function tanggal() {
-  console.log("Mencetak Tanggal Sekarang: " + new Date());
-}
-tanggal();
-
-function tanggal1(tanggal) {
-  console.log("Memberikan Tanggal Sekarang: " + tanggal);
-}
-tanggal1(new Date());
