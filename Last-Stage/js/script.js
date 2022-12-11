4deda3176999728cd4af8d1cfe46a71ed276ba8b
pucuk1337 <pucuk4us@gmail.com>
2022-12-11 22:18:33 +0700

    Update Responsive and JS for Object

--- Last-Stage/js/script.js
+++ Last-Stage/js/script.js
@@ -1,1 +1,30 @@
 AOS.init();
+
+let obj = {
+  key: "5b0fd9b6f4abbe5b87b37b997f57e5c4",
+  // GET API TO JSON
+  fetch_weather: function (city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.key)
+      .then((response) => response.json())
+      .then((data) => this.show_weather(data));
+    // .then((data) => console.log(data));
+  },
+  // JSON TO DISPLAY
+  show_weather: function (data) {
+    let { name } = data;
+    let { description } = data.weather[0];
+    let { temp } = data.main;
+    let { speed } = data.wind;
+    document.querySelector("#city").innerText = name;
+    document.querySelector("#description").innerText = description;
+    document.querySelector("#temperature").innerText = temp;
+    document.querySelector("#windspeed").innerText = speed;
+  },
+  submit_weather: function () {
+    this.fetch_weather(document.querySelector("#inputWilayah").value);
+  },
+};
+
+document.querySelector("#submit").addEventListener("click", function (e) {
+  obj.submit_weather(e);
+});
